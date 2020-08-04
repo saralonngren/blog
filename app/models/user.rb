60d7580d6d2be017f4c0e89dd81fde550e722907
Sara Lonngren <saralonngren@gmail.com>
2020-08-04 07:26:24 -0300

    created navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@
 #  updated_at             :datetime         not null
 #
 class User < ApplicationRecord
+  has_one_attached :avatar
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
