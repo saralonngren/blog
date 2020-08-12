87a8b528fce067c4cbecd44460bddc2577e02c56
Sara Lonngren <saralonngren@gmail.com>
2020-08-12 07:47:02 -0300

    Created join table to posts and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_124345) do
+ActiveRecord::Schema.define(version: 2020_08_12_104351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,11 @@ ActiveRecord::Schema.define(version: 2020_08_08_124345) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "color"
+  end
+
+  create_table "categories_posts", id: false, force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "category_id", null: false
   end
 
   create_table "posts", force: :cascade do |t|
