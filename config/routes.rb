b02c671fc485084946e50e24ef023d24c19f6f8f
Sara Lonngren <saralonngren@gmail.com>
2020-08-19 22:59:38 -0300

    Initialized readme page

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :categories
   devise_for :users
+
+  get 'readme', to: 'abouts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
